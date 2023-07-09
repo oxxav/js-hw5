72f06f77261aeac0c83c31af31a35a2528183604
[oxxav] <[woxxav@gmail.com]>
2023-07-09 19:31:32 +0300

    hw5.3

---
+++ task_score.js
@@ -0,0 +1,15 @@
+let introduction = 180;
+let git = 189;
+let js = 260;
+
+let total = introduction + git + js;
+console.log(total);
+let average = total / 3;
+average = Math.ceil(average);
+console.log(average);
+
+let newName = "Oksana";
+let surname = "Savoskina";
+let fullName = newName + " " + surname;
+
+console.log(`Average value ${average} for ${fullName} `);
